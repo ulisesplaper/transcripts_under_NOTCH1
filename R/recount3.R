014eadfd690686aae5b5c47ca53a51208452f37f
Ulisesplapler <ulisesp@lcg.unam.mx>
2023-02-12 16:51:30 -0600

    Initial commit

---
+++ R/recount3.R
@@ -0,0 +1,43 @@
+## Download experiment data and generate
+# RangeSummarizedObject
+
+# Import packages
+library("recount3")
+
+
+## Current url
+getOption(
+  "recount3_url",
+  "http://duffel.rail.bio/recount3"
+)
+
+## Change URL
+options(recount3_url = "https://recount-opendata.s3.amazonaws.com/recount3/release")
+
+## Check changes.
+getOption(
+  "recount3_url",
+  "http://duffel.rail.bio/recount3"
+)
+
+## Check human projects
+human_projects <- available_projects(organism = 'human')
+
+## Find project of interest
+proj_info <- subset(
+  human_projects,
+  project == "SRP048604" & project_type == "data_sources"
+)
+## Create RangedSummarizedExperiment (RSE) object
+## with project info
+rse_gene_SRP048604 <- create_rse(proj_info)
+
+# Explore RSE
+rse_gene_SRP048604
+
+#Scale coverage counts
+
+assay(rse_gene_SRP048604, "counts") <- compute_read_counts(rse_gene_SRP048604)
+
+# Save the scaled rse
+save(rse_gene_SRP048604, file = 'data/rse_gene_SRP048604.RData')
